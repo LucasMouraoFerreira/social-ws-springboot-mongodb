659507e5c961ce1ad79299267cc593a3a373e7d9
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-02-09 13:07:36 -0300

    Project created

---
+++ src/main/java/com/lucasmourao/socialmongodb/SocialMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmourao.socialmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmourao/socialmongodb/SocialMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmourao.socialmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
