77c2f04906b6fddd1c94d8127ed5ad4b06d8bb75
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-02-10 19:44:29 -0300

    User post

--- src/main/java/com/lucasmourao/socialmongodb/services/UserService.java
+++ src/main/java/com/lucasmourao/socialmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasmourao.socialmongodb.domain.User;
+import com.lucasmourao.socialmongodb.dto.UserDTO;
 import com.lucasmourao.socialmongodb.repositories.UserRepository;
 import com.lucasmourao.socialmongodb.services.exceptions.ObjectNotFoundException;
 
@@ -24,7 +25,14 @@ public class UserService {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
-
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(),objDTO.getName(),objDTO.getEmail());
+	}
  
 	
 }
