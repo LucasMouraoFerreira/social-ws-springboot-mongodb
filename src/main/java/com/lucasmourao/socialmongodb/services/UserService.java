1928cbd97fda5faf3f6858976fc4859342a2b86b
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-02-10 19:28:28 -0300

    User findById and NotFoundException

--- src/main/java/com/lucasmourao/socialmongodb/services/UserService.java
+++ src/main/java/com/lucasmourao/socialmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.lucasmourao.socialmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasmourao.socialmongodb.domain.User;
 import com.lucasmourao.socialmongodb.repositories.UserRepository;
+import com.lucasmourao.socialmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,12 @@ public class UserService {
 	public List<User> findAll(){
 		return repository.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+ 
 	
 }
