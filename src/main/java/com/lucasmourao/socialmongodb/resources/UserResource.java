f932d57cace10c2f587b5368f972732f77a97e9a
LucasMouraoFerreira <lucas.mo.ferr@gmail.com>
2020-02-11 18:59:05 -0300

    Posts get method

--- src/main/java/com/lucasmourao/socialmongodb/resources/UserResource.java
+++ src/main/java/com/lucasmourao/socialmongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.lucasmourao.socialmongodb.domain.Post;
 import com.lucasmourao.socialmongodb.domain.User;
 import com.lucasmourao.socialmongodb.dto.UserDTO;
 import com.lucasmourao.socialmongodb.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		user.setId(id);
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts",method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 }
